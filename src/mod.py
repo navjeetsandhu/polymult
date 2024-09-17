9cf48313bbf7041d1fef8e417bda1ac74be353ce
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-09-17 15:02:43 -0400

    added modulus

---
+++ src/mod.py
@@ -0,0 +1,29 @@
+from sympy import isprime
+
+
+#  Biggest prime number less than 2^bits and satisfies 1 mod 2N.
+# where bits is modulus bits.  i.e. HEXL support Maximum number of 62 bits in modulus
+# N is dimensions. i.e. for tfhe lvl1param, the dimension is 1024, i.e. 1024 elements in polynomial
+def print_mod(bits, dimension):
+    # Number to start checking
+    n = 2 ** bits - 1
+
+    d2 = 2 * dimension;
+
+    # Limit is smallest odd number that satisfies the condition.
+    limit = d2 * 2 + 1
+
+    # Loop while we are above the limit
+    while n >= limit:
+        # If number is a prime and satisfies the condition n congruent 1 (mod 2048)
+        if isprime(n) and n % d2 == 1:
+            print("bits %d, dimension %d Modulus = %d (%s)" % (bits, dimension, n, hex(n)))
+            break
+        # Decrease number by 2 to take the next lower odd number
+        n -= 2
+
+
+if __name__ == '__main__':
+    print_mod(62, 1024)
+    print_mod(30, 1024)
+    print_mod(30, 4)
--- src/poly.py
+++ src/poly.py
@@ -1,5 +1,4 @@
-
-def horners_method(coeffs, x, k = 0):
+def horners_method(coeffs, x, k=0):
     """
     Evaluate horners_method for a polynomial with coefficients 'coeffs'
     for the value x, starting at position k.
@@ -7,32 +6,35 @@ def horners_method(coeffs, x, k = 0):
     if k == len(coeffs) - 1:
         return coeffs[k]
     else:
-        return coeffs[k] + x * horners_method(coeffs, x, k+1)
+        return coeffs[k] + x * horners_method(coeffs, x, k + 1)
+
 
 def to_point_value(coeffs, x_pts):
     result = []
     for k in range(0, len(coeffs)):
-        result.append( (x_pts[k], horners_method(coeffs, x_pts[k])) )
+        result.append((x_pts[k], horners_method(coeffs, x_pts[k])))
     return result
 
 
-C_e = [1,2,3]
-C_a = [4,-3, 2, -4]
-C_b = [1, 7, 2,  3]
-X = [4,5,6]
+def test_one():
+    # Example usage
+    C_e = [1, 2, 3]
+    C_a = [4, -3, 2, -4]
+    C_b = [1, 7, 2, 3]
+    X = [4, 5, 6]
 
-X1 = [4, 3, 9, -3]
+    X1 = [4, 3, 9, -3]
 
-C_c = [ C_a[i] + C_b[i] for i in range(0, len(C_a)) ]
+    C_c = [C_a[i] + C_b[i] for i in range(0, len(C_a))]
 
+    A = to_point_value(C_a, X1)
+    B = to_point_value(C_b, X1)
+    C = to_point_value(C_c, X1)
 
-
-A = to_point_value(C_a, X1)
-B = to_point_value(C_b, X1)
-C = to_point_value(C_c, X1)
-
-print("A", A)
-print("B", B)
-print("C", C)
+    print("A", A)
+    print("B", B)
+    print("C", C)
 
 
+if __name__ == '__main__':
+    test_one()
