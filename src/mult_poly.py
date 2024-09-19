d8244b97ccfc0ac2b9222f1fc00f65cbb9be4d38
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-09-19 15:23:22 -0400

    Integers modulo a prime number q

--- src/mult_poly.py
+++ src/mult_poly.py
@@ -1,4 +1,8 @@
+
 # https://cryptographycaffe.sandboxaq.com/posts/ntt-01/
+
+
+# Schoolbook Multiplication
 # Adding two polynomials is as simple as just summing up their coefficients.
 # Multiplying a polynomial by a constant is just multiplying every coefficient by that constant.
 # However, when we want to multiply polynomials, things get slightly more complicated.
@@ -11,15 +15,32 @@ def mult_poly_naive(p1, p2):
     return result
 
 
-def test_mult_poly_naive():
+
+# integers modulo a prime number q
+# The interesting thing about working in a finite field is that the polynomial coefficients
+# “wrap around” when being multiplied. So regardless of how much polynomial arithmetic we
+# perform, the coefficients of the polynomial can still be bounded in a fixed range
+
+def mul_poly_naive_q(a, b, q):
+    tmp = [0] * (len(a) + len(b) - 1)  # the product of two polynomials cannot exceed the sum of their degree
+
+    # schoolbook multiplication
+    for i in range(len(a)):
+        # perform a_i * b
+        for j in range(len(b)):
+            tmp[i + j] = (tmp[i + j] + a[i] * b[j]) % q
+
+    return tmp
+
+
+def test_mult_poly():
     a = [1, 2, 3, 4]
     b = [1, 3, 5, 7]
+    print ("Schoolbook Multiplication: ")
     print(mult_poly_naive(a, b))
     # [1, 5, 14, 30, 41, 41, 28]
-
-
-
-
+    print("Integers modulo a prime number q: ")
+    print(mul_poly_naive_q(a, b, 17))
 
 if __name__ == '__main__':
-    test_mult_poly_naive()
+    test_mult_poly()
